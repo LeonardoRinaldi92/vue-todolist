5662c22642e06bbec6330e60598809ed8579f657
LeonardoRinaldi92 <leonardo.rinaldi.ph@gmail.com>
2023-04-20 14:35:59 +0200

    completo

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,27 +15,70 @@ createApp({
             {
                 text: 'Fare il bucato',
                 done: false
-            }
+            },
+            {
+                text: 'Rapinare banca',
+                done: true
+            },
+            {
+                text: 'Aggiungere Bonus',
+                done: true
+            },
+
         ],
 
         newTodo : {
             text:'',
-            done: false,
-        }
+            done: false
+        },
+
+        errore : false
     }
   },
   methods: {
     addTodo() {
-        this.todos.unshift(this.newTodo),
-        this.newTodo = {
-            text:'',
-        done: false,
-        }
+        if (this.newTodo.text !== '' && this.newTodo.text.length > 4){
+            this.todos.unshift(this.newTodo),
+            this.newTodo = {
+                text:'',
+            done: false,
+            }
+            this.errore = false
+        }else{
+            this.errore = true
+        } 
     },
+
     eliminaPost(i) {
         this.todos.splice(i , 1)
     },
+
+    randomColor() {
+        let colore = Math.floor(Math.random() * 4)
+        if (colore == 0){
+            return "giallo"
+        }else if (colore == 1) {
+            return "verde"
+        }else if (colore == 2) {
+            return "celeste"
+        }else if (colore == 3) {
+            return "rosa"
+        }
+    },
+
+    randomPosition(){
+        let angolo = Math.floor(Math.random() * 2)
+        let numero = Math.floor(Math.random() * 30) + 1
+        if (angolo == 1) {
+            angolo = "+" 
+        } else {
+            angolo = "-"
+        }
+        return "transform: rotate("+angolo+numero+"deg)"
+
+
     }
+  },
 }
 
 ).mount('#app')
